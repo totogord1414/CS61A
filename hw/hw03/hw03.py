257113a0909a515b3704d090cceddf1d4c499e49
totogord1414 <zhanzitong021@gmai.com>
2026-03-22 16:51:44 +0800

    3,222

--- hw/hw03/hw03.py
+++ hw/hw03/hw03.py
@@ -243,3 +243,24 @@ def make_anonymous_factorial():
     """
     return  
 
+
+
+
+from operator import sub, mul
+
+def Y(f):
+    """The Y ("paradoxical") combinator."""
+    return f(lambda: Y(f))
+
+def Y_tester():
+    """
+    >>> tmp = Y_tester()
+    >>> tmp(1)
+    1
+    >>> tmp(5)
+    120
+    >>> tmp(2)
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    return Y(____________________)  # Replace 
