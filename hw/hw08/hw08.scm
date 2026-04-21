68e48fbee39af245ff199f8d9078b05939a6f07c
totogord1414 <zhanzitong021@gmail.com>
2026-04-21 16:53:00 +0800

    4.21

---
+++ hw/hw08/hw08.scm
@@ -0,0 +1,19 @@
+(define (ascending? s)
+    (if (or (null? s) (null? (cdr s))) #t
+        (and (<= (car s) (car (cdr s))) (ascending? (cdr s)))) )
+
+(define (my-filter pred s) 
+    ( if (null? s) s
+        (if (pred (car s))
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2) (
+    cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
+
+(define (no-repeats s) 
+    (if (null? s) s
+        (cons (car s) (no-repeats(filter (lambda (x) (not (= x (car s)))) s)))))
